64cd547168cb8e24324cab76449350d6ce290dd6
simon <sdiaz.sz@gmail.com>
2020-08-21 16:51:58 -0400

    clean up

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,5 @@ Route::group([
     'as'         => 'larangular.api.web-service-client.',
 ], function () {
     Route::get('service/{service}/{quote}', 'Request\Gateway@makeRequest');
-
     Route::get('service/{service}/{quote}/request{extension?}', 'Request\Gateway@getRequest');
-
 });
