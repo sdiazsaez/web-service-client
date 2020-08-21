e0a9adfb1f228504bb26b97fed3e5fe090d26bd5
simon <sdiaz.sz@gmail.com>
2020-08-21 16:54:57 -0400

    route fix

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ Route::group([
     'middleware' => 'web',
     'namespace'  => 'Larangular\WebServiceClient\Http\Controllers',
     'as'         => 'larangular.api.web-service-client.',
-], function () {
-    Route::get('service/{service}/{quote}', 'Request\Gateway@makeRequest');
-    Route::get('service/{service}/{quote}/request{extension?}', 'Request\Gateway@getRequest');
+], static function () {
+    Route::get('service/{service}/{quote}', 'WebServiceClient\Gateway@makeRequest');
+    Route::get('service/{service}/{quote}/request{extension?}', 'WebServiceClient\Gateway@getRequest');
 });
